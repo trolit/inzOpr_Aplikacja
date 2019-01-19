a0c35aebbea9c181dd12ac9e9d6c8165bc4a7634
damianggg <46781002+damianggg@users.noreply.github.com>
2019-01-19 12:25:42 +0100

    ­

--- crud_fux/crud/Models/Model1.Designer.cs
+++ crud_fux/crud/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Github\inzOpr_Aplikacja\crud_fux_y\crud\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Damian\Documents\GitHub\inzOpr_Aplikacja\crud_fux\crud\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
